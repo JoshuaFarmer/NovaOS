2201b977dfb2e795891735cb8d14f111f2043e47
Joshua Farmer <rhubarbjosh@outlook.com>
2024-06-09 21:29:41 +0100

    malloc

--- src/C/kernel32.c
+++ src/C/kernel32.c
@@ -18,7 +18,6 @@ void kernel_main() {
 	// set_text_mode(1);
 	// VGA_WIDTH = 90;
 	// VGA_HEIGHT = 60;
-	void* vd = malloc(1024);
 
 	identify_ata(0xA0);
 	system_user = malloc(64);
@@ -29,8 +28,12 @@ void kernel_main() {
 	puts_coloured("#############\n", VGA_COLOR_WHITE);
 	puts_coloured("#############\n", VGA_COLOR_LIGHT_RED);
 	puts_coloured("#############\n", VGA_COLOR_LIGHT_CYAN);
-	
+
 	fat_BS_t* bs = malloc(512);
+	uint16_t* kbdbuf = malloc(128 * sizeof(uint16_t));
+	uint8_t* current_dir = malloc(12);
+	strcpy((char*)current_dir, "/root/");
+	
 	read_boot_sector(0xA0, bs);
 	printf("%T10.bytes_per_sector: %T14.%d\n", bs->bytes_per_sector);
     printf("%T10.sectors_per_cluster: %T14.%d\n", bs->sectors_per_cluster);
@@ -39,11 +42,6 @@ void kernel_main() {
     printf("%T10.total_sectors_16: %T14.%d\n", bs->total_sectors_16);
     printf("%T10.total_sectors_32: %T14.%d\n", bs->total_sectors_32);
     printf("%T10.table_size_16: %T14.%d\n", bs->table_size_16);
-
-	uint16_t* kbdbuf = malloc(128 * sizeof(uint16_t));
-
-	uint8_t* current_dir = malloc(12);
-	strcpy((char*)current_dir, "/root/");
 
 	size_t heap_size = remaining_heap_space();
 	printf("%T10.heap size:%T14. %d\n", HEAP_CAP);
--- src/C/malloc.h
+++ src/C/malloc.h
@@ -2,76 +2,70 @@
 #include <stdint.h>
 #include "memory.h"
 
-#define HEAP_CAP 1024 * 256 // 256k should be enough for everyone, right?
+#define HEAP_CAP (1024 * 120) // 120k should be enough for everyone, right?
 
 void puts_coloured(const char* data, const uint8_t col);
 
-typedef struct HEAP {
-	uint8_t  raw[HEAP_CAP];
-	uint8_t  is_free[HEAP_CAP];
-	uint16_t len[HEAP_CAP];
+typedef struct {
+    uint8_t  raw[HEAP_CAP];
+    uint16_t next_free;
+    uint16_t len[HEAP_CAP];
 } Heap_t;
 
-Heap_t heap;
+Heap_t heap = { .next_free = 0 };
 
 void init_heap() {
-    memset(heap.is_free, true, HEAP_CAP);
     memset(heap.len, 0, sizeof(heap.len));
 }
 
-// VERY BAD - but fuck it
 void* malloc(size_t size) {
-	if (size == 0 || size > HEAP_CAP) {
+    if (size == 0 || size > HEAP_CAP) {
         puts_coloured("MALLOC - INVALID SIZE\n", VGA_COLOR_LIGHT_RED);
         return NULL;
     }
 
-    for (size_t i = 0; i < HEAP_CAP; ++i) {
-        if (heap.is_free[i]) {
+    size_t i = heap.next_free;
+    while (i < HEAP_CAP) {
+        if (heap.len[i] == 0) { // Free block
             size_t free_block_size = 0;
-
-            for (size_t j = i; j < HEAP_CAP && heap.is_free[j]; ++j) {
+            for (size_t j = i; j < HEAP_CAP && heap.len[j] == 0; ++j) {
                 ++free_block_size;
                 if (free_block_size == size) {
-                    for (size_t k = i; k < i + size; ++k) {
-                        heap.is_free[k] = false;
-                    }
                     heap.len[i] = size;
+                    heap.next_free = i + size;
                     return &heap.raw[i];
                 }
             }
-            i += free_block_size;
         }
+        i += heap.len[i] ? heap.len[i] : 1;
     }
     puts_coloured("MALLOC - NO SPACE IN HEAP\n", VGA_COLOR_LIGHT_RED);
     return NULL;
 }
 
-// VERY BAD - but fuck it
 void free(void* ptr) {
-	if (ptr == NULL || ptr < (void*)heap.raw || ptr >= (void*)(heap.raw + HEAP_CAP)) {
+    if (ptr == NULL || ptr < (void*)heap.raw || ptr >= (void*)(heap.raw + HEAP_CAP)) {
         puts_coloured("FREE - INVALID POINTER\n", VGA_COLOR_LIGHT_RED);
         return;
     }
 
     size_t offset = (uint8_t*)ptr - heap.raw;
-    size_t length = heap.len[offset];
-
-    if (length == 0) {
+    if (heap.len[offset] == 0) {
         puts_coloured("FREE - NOT FOUND IN THE HEAP\n", VGA_COLOR_LIGHT_RED);
         return;
     }
 
-    memset(&heap.is_free[offset], true, length);
+    heap.next_free = offset < heap.next_free ? offset : heap.next_free;
+    memset(&heap.raw[offset], 0, heap.len[offset]);
     heap.len[offset] = 0;
 }
 
 size_t remaining_heap_space() {
-    size_t remain = 0;
-    for (int i = 0; i < HEAP_CAP; ++i) {
-        if (heap.is_free[i]) {
-            ++remain;
+    size_t taken = HEAP_CAP;
+    for (size_t i = 0; i < HEAP_CAP; i += heap.len[i] ? heap.len[i] : 1) {
+        if (heap.len[i] == 0) {
+            taken -= 1; // Only count the starting byte of a free block
         }
     }
-    return remain;
+    return HEAP_CAP - taken;
 }
