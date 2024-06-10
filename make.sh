1db395c45b41f34fbabf2636cca8d55d989b37f6
Joshua Farmer <rhubarbjosh@outlook.com>
2024-06-10 17:51:31 +0100

    -Werror

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ find . \( -name '*.c' -or -name '*.h' \) -print0 | xargs -0 wc -l
 
 # Compile assembly and C source files
 clang -m32 -c "src/asm/boot.s" -o "bin/boot.o" -march=i386
-clang -m32 -c -ffreestanding "src/C/kernel32.c" -o "bin/krnl32.o" -Wall -Wextra -march=i386
+clang -m32 -c -ffreestanding "src/C/kernel32.c" -o "bin/krnl32.o" -Wall -Wextra -Werror -march=i386
 
 # Link the object files to create the binary
 ld -m elf_i386 -T linker.ld -o bin/Nova.bin -O2 -nostdlib "bin/boot.o" "bin/krnl32.o"
