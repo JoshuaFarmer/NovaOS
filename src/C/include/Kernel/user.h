1d9890419e4605f4a1eae538f18263bb6e2144f3
Joshua Farmer <rhubarbjosh@outlook.com>
2024-07-10 17:57:19 +0100

    perms

--- src/C/include/Kernel/user.h
+++ src/C/include/Kernel/user.h
@@ -5,7 +5,10 @@
 
 typedef enum {
 	ROOT, // root
-	MOD, // moderator, has some root commands, but not all
+	MOD,  // moderator, has some root commands,
+	      // but not all (in linux this would be
+			// kinda like "SUDO" but with some
+			// functions still restricted, e.g. rm -rf /)
 	USER,
 } perms_t;
 
