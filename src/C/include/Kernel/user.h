3fd903bb9c4e0284887e32ef96e541a091fc74bf
Joshua Farmer <rhubarbjosh@outlook.com>
2024-07-10 17:58:27 +0100

    tabs

--- src/C/include/Kernel/user.h
+++ src/C/include/Kernel/user.h
@@ -5,8 +5,8 @@
 
 typedef enum {
 	ROOT, // root
-	MOD,  // moderator, has some root commands,
-	      // but not all (in linux this would be
+	MOD,	// moderator, has some root commands,
+			// but not all (in linux this would be
 			// kinda like "SUDO" but with some
 			// functions still restricted, e.g. rm -rf /)
 	USER,
